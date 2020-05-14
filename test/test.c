51a7ada1a85bbdea692a502fddbb20aa23cf357b
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-05-14 12:27:44 +0530

    added tests for create_list and create_node

---
+++ test/test.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "../linkedlist.h"
+#include "../utils.h"
+#include "test.h"
+
+int assert(int actual, int expected)
+{
+  return actual == expected;
+}
+
+void display_pass_or_fail(int status)
+{
+  status ? printf(PASS) : printf(FAIL);
+  status ? PASSING_TESTS++ : FAILING_TESTS++;
+}
+
+void test_create_list(void)
+{
+  PRINT_STRING("\ncreate_list");
+
+  List_ptr list = create_list();
+  Status status = list != NULL;
+  status = status && list->first == NULL;
+  status = status && list->last == NULL;
+  display_pass_or_fail(status);
+  PRINT_STRING("should create a list and set its first and last to NULL");
+}
+
+void test_create_node(void)
+{
+  PRINT_STRING("\ncreate_node");
+
+  Element element = create_int_element(1);
+  Node_ptr node = create_node(element);
+  Status status = node != NULL;
+  status = status && node->element == element;
+  status = status && node->next == NULL;
+  display_pass_or_fail(status);
+  PRINT_STRING("should create a node and set its element to given element and next to NULL");
+}
+
+int main(void)
+{
+  List_ptr list = create_list();
+  if (list == NULL)
+  {
+    PRINT_STRING("Memory_Not_Available");
+    return 0;
+  }
+
+  test_create_list();
+  test_create_node();
+
+  printf(GREEN "\n%d passing" RESET, PASSING_TESTS);
+  printf(RED "\n%d failing\n" RESET, FAILING_TESTS);
+
+  return 0;
+}
---
+++ test/test.h
@@ -0,0 +1,24 @@
+#ifndef __TEST_H
+#define __TEST_H
+
+#define PRINT_STRING(string) printf("%s\n", string)
+
+// ✔ = \u2714  HEAVY CHECK MARK
+// ✘ = \u2718  HEAVY BALLOT X
+
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define PASS GREEN "\u2714 " RESET
+#define FAIL RED "\u2718 " RESET
+
+int PASSING_TESTS = 0;
+int FAILING_TESTS = 0;
+
+int assert(int actual, int expected);
+void display_pass_or_fail(int status);
+
+void test_create_list(void);
+void test_create_node(void);
+
+#endif
