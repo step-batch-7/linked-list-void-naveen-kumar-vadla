811f5b02d36ca82fb65ebc048992ad8d292906d4
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-05-14 08:18:05 +0530

    added script to run the program and added outputs to .gitignore

---
+++ run.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+directory=$1
+executable=$2
+
+mkdir -p outputs
+rm -rf $directory/*.o
+gcc -c linkedlist.c $directory/*.c
+gcc -o outputs/$executable *.o && outputs/$executable
+rm -rf *.o
